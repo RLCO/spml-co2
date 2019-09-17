8c42179a452dd142733d34a043fb3e960cf4cb7e
telenocodoo <telenocodoo>
2019-09-17 18:53:39 +0300

    third

--- sprogroup_purchase_request/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- encoding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import sprogroup_purchase_request
-from . import res_partner
-from . import quality_purchase
